f1f2524c0a7ba5916d8687524f441fed9491b90c
rparkar <rparkar@RENTLAPT0011>
2020-02-04 13:43:54 +0530

    Hoisting for function declarations, function expressions and variables

--- how-js-works/script.js
+++ how-js-works/script.js
@@ -1,25 +1,35 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//functions
+//This function execution(calculateAge) works fine as Hoisting works for function declaration.
+calculateAge(1997);
 
+function calculateAge(year) {
+  console.log(2020 - year);
+}
 
+//This function execution(retirement) throws an error as Hoisting will not work for function expressions.
+//retirement(1990);
 
+var retirement = function(year) {
+  console.log(65 - (2020 - year));
+};
 
+//variables
+//The following would show result as undefined since Javascript knows that a variable named 'age' exists(by the concept of Hoisting) but when it is logged, it's value is not set.
+console.log(age);
+var age = 23;
 
-
-
-
-
-
-
-
-
-
-
+function foo() {
+  var age = 65;
+  console.log(age); //prints 65, as the scope of age here is limited to function foo.
+}
+foo();
+console.log(age); //prints 23, as the scope of age here is global.
 
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -37,8 +47,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +70,5 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
-
-
-
-
-
-
-
-
-
